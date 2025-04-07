318d85f6fd2bdf9b6dbf2683f7dac5ea3857cdeb
Edgar Santiago Ariza Garcia <edgarariza6@outlook.com>
2025-04-07 15:05:40 -0500

    Empezamos javascript
    Javascript dalto

---
+++ JavaScript/Inicio/Inicio.js
@@ -0,0 +1,36 @@
+/* Esta es la forma de comentar en javascript */
+
+/* Tipos de Variables */
+
+/* hay tres formas de llamar los datos que son var, let y const. */
+
+string = "cadena de texto";
+number = 0, 1, 2, 3, 4;
+booleano = true, false;
+
+var numero1 = 15;
+let numero2 = 15;
+const numero3 = 15;
+
+/* Casos especiales de datos */
+
+let numero = null; /* el primer valor es null y significa que esta vacia */
+let nombre = undefined; /* este valor signifia que no esta definida pero no es igual a vacia */
+let apellido = NaN; /* significa que la variable no es numero y no se puede concatenar como tal */
+
+/* operadores y concanetacion */
+
+let name1 = prompt("dime tu nombre");
+
+alert("Hola " + name1);
+
+/* operadores algebraicos */
+
+let num = 10;
+num += 100;
+num -= 1;
+num /= 1;
+num *= 2;
+num %= 3;
+num **= 2;
+num 
